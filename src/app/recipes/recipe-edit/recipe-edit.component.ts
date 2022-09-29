05418c6bb33878f86a9485366e5cfc329c4cd415
felix fedronic <fedronicfelix@gmail.com>
2022-09-29 08:36:25 +0200

    recipe edit form: reactive form + ingredients Ctrl

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,3 +1,5 @@
+import { RecipeServices } from './../recipes.services';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,14 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
   id!: number;
+
   editMode = false;
 
-  constructor(private route: ActivatedRoute) {}
+  recipeForm!: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeServices
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
+
       this.editMode = params['id'] != null;
+
+      this.initForm();
     });
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray<FormGroup>([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getOneRecipe(this.id);
+
+      recipeName = recipe.name;
+      recipeImagePath = recipe.description;
+      recipeDescription = recipe.description;
+
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
+    }
+    
+    this.recipeForm = new FormGroup({
+      inputName: new FormControl(recipeName),
+      inputImagePath: new FormControl(recipeImagePath),
+      inputDescription: new FormControl(recipeDescription),
+      ingredients: recipeIngredients,
+    });
+  }
+
+  get controls() { // a getter!
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
-import { RecipeServices } from './../recipes.services';
-import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+
+import { RecipeServices } from './../recipes.services';
+import { Recipe } from './../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
+
 export class RecipeListComponent implements OnInit {
   recipes!: Recipe[];
   
